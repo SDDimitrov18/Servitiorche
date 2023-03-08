8c53d7138bb1d34b20e09428c0f051064e894501
SDDimitrov18 <SDDimitrov18@codingburgas.bg>
2023-03-08 14:40:00 +0200

    Started developing the back end of the waiter programme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
-    }
-}
+
+        int waiter = 1;
+
+        System.out.println("Waiter " + waiter + " is serving your food");
+}}
